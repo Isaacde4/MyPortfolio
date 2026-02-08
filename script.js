ba82e6b4fdc0de906f96708a3445bea2a62b73d4
Isaac Ataaba <isaaclistowella@gmail.com>
2026-02-08 12:53:07 +0200

    My portfoliochanges

---
+++ script.js
@@ -0,0 +1,17 @@
+window.onload = () => {
+    const title = document.getElementById('welcomeTitle');
+    const hour = new Date().getHours();
+    let greeting = '';
+
+    if (hour >= 5 && hour < 12) {
+        greeting = 'Good Morning';
+    } else if (hour >= 12 && hour < 18) {
+        greeting = 'Good Afternoon';
+    } else {
+        greeting = 'Good Evening';
+    }
+
+    if (title) {
+        title.textContent = greeting;
+    }
+};
